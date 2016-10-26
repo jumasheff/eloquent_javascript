8cebdb1d45f304118cf53c809f113ba10d881b0e
muratjumashev <jumasheff@gmail.com>
2016-10-26 10:03:09 +0600

    Add deepEqual

--- chapter_04.js
+++ chapter_04.js
@@ -13,3 +13,32 @@ function nthRecursive(list, position) {
 var l = {value: 10, rest: {value: 20, rest: {value: 30, rest: null}}};
 console.log(nthRecursive(l, 1));
 
+function deepEqual(obj1, obj2) {
+  var valuesAreObjects = function(val1, val2) {
+    return typeof val1 === "object" && typeof val2 === "object"
+  }
+  
+  for (key in obj1) {
+    if (!key in obj2) {
+      return false
+    }
+    var obj1Key = obj1[key];
+    var obj2Key = obj2[key];
+    
+    if (valuesAreObjects(obj1Key, obj2Key)) {
+      deepEqual(obj1Key, obj2Key)
+    } else {
+      if(obj1Key !== obj2Key)
+        return false
+    }
+  }
+  return true
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
